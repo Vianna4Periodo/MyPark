1f1bd7fccb948549e3fa24a4c88a28301ec80bae
Felipe Ferraz <felipe.ferraz.developer@gmail.com>
2018-03-20 11:12:42 -0300

    First commit

---
+++ MyPark.Model/DataBase/Repository/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using MyPark.Model.DataBase.Models;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPark.Model.DataBase.Repository
+{
+    public class UserRepository : RepositoryBase<User>
+    {
+        public UserRepository(ISession session) : base(session) { }
+
+    }
+}
