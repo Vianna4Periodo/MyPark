f32d231799a10b308f2dcdec5cb4e6148633aa5b
aluno <aluno@labivj.info>
2018-03-22 19:24:14 -0300

    Criação do modelo 'Livro'

---
+++ MyPark.Model/DataBase/Models/Livro.cs
@@ -0,0 +1,32 @@
+﻿using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPark.Model.DataBase.Models
+{
+    public class Livro
+    {
+        public virtual Guid Id { get; set; }
+        public virtual String Titulo { get; set; }
+        public virtual String Autor { get; set; }
+        public virtual String ISBN { get; set; }
+        public virtual String Editora { get; set; }
+    }
+
+    public class LivroMap: ClassMapping<Livro>
+    {
+        public LivroMap() {
+            Id(x => x.Id, m => m.Generator(Generators.Guid));
+
+            Property(x => x.Titulo);
+            Property(x => x.Autor);
+            Property(x => x.ISBN);
+            Property(x => x.Editora);
+        }
+    }
+
+}
