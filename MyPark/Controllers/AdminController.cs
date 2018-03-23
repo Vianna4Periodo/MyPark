30b63bb04809c6a67fe13c3e5ae7f9042edc69ac
aluno <aluno@labivj.info>
2018-03-22 21:52:53 -0300

    Iniciando criação do controller de login

--- MyPark/Controllers/AdminController.cs
+++ MyPark/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyPark.Model.DataBase;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,7 @@ namespace MyPark.Controllers
         // GET: Admin
         public ActionResult TipoVeiculo()
         {
+            var tipoVeiculos = DbFactory
             return View();
         }
     }
